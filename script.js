e497953a6b6d1762c9bca4e9a269e8bae7bbaf69
oat phattaraphon <phattaraphon.c@ku.th>
2020-09-06 22:57:31 +0700

    finished function createSnowBalls and random

---
+++ script.js
@@ -0,0 +1,34 @@
+(() => {
+  function setup() {
+    const canvas = document.getElementById('falling-snow-canvas');
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+
+    return {
+      canvas,
+      canvasContext: canvas.getContext('2d'),
+      numberOfSnowBalls: 250,
+    };
+  }
+
+  function createSnowBalls(canvas, numberOfSnowBalls) {
+    return [...Array(numberOfSnowBalls)].map(() => {
+      return {
+        x: random(0, canvas.width),
+        y: random(0, canvas.height),
+      };
+    });
+    // console.log(x);
+  }
+
+  function random(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
+
+  function run() {
+    const { canvas, canvasContext, numberOfSnowBalls } = setup();
+    createSnowBalls(canvas, numberOfSnowBalls);
+  }
+
+  run();
+})();
